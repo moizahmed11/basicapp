12429d8f7737f8176c02df43a5ab6f3dee475eaf
moiz.ahmed <moiz.ahmed@sybrid.com>
2023-11-14 00:54:31 +0500

    States

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import logo from './logo.svg';
+import {React, useState} from 'react'; 
 import './App.css';
-
+import Button from "react-bootstrap/Button";
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  
+  // let counter = 15 
+  // const addValue = () => {
+  //     counter = counter + 1; 
+  //     console.log(counter);
+  // }
+
+  const [counter, setCounter] = useState(1)
+
+   const addValue = () => {
+        setCounter(counter + 1)
+   }
+ 
+   const subValue = () => {
+    setCounter(counter - 1)
+   }
+
+  return (  
+    <>  
+      <header className="App-header"> 
+      <Button onClick={addValue}>Add Value</Button>
+      <Button onClick={subValue}>Remove</Button>
+        <p>{counter} Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,10 +33,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React one two
+           
         </a>
       </header>
-    </div>
+    </>
   );
 }
 
